f64361e9cbd4a1f7b97f9e4021a31404f656bdaf
Will Hardwick-Smith <whardwicksmith@gmail.com>
2020-01-26 15:01:00 +1300

    shape now can't go out of bounds

--- tetris.hs
+++ tetris.hs
@@ -9,7 +9,7 @@ import System.Random
 rows = 5
 cols = 8
 
-data Action = Left | Right | Up | Down | None
+data Action = Left | Right | Up | Down | None deriving Eq
 data Point = Point Int Int deriving Show
 data Player = Player Point [[Bool]] deriving Show
 
@@ -42,12 +42,12 @@ game_tick board player = do
 
     action_str <- getLine
     let action = parse_action action_str
-    let moved_player = move_player player action
+    let moved_player = move_player player action board
 
     random_gen <- newStdGen
     let (updated_board, updated_player) = game_update board moved_player random_gen
 
-    if is_game_over board updated_player then do
+    if is_player_on_brick board updated_player then do
         putStrLn "game over man"
     else do
         game_tick updated_board updated_player
@@ -115,12 +115,21 @@ parse_action "e" = Main.Up
 parse_action "d" = Main.Down
 parse_action str = None
 
-move_player :: Player -> Action -> Player
-move_player (Player (Point row col) shape) Main.Left = Player (Point row (col-1)) shape
-move_player (Player (Point row col) shape) Main.Right = Player (Point row (col+1)) shape
-move_player (Player (Point row col) shape) Main.Up = Player (Point (row-1) col) shape
-move_player (Player (Point row col) shape) Main.Down = Player (Point (row+1) col) shape
-move_player player None = player
+move_player :: Player -> Action -> [[Bool]] -> Player
+move_player player action board
+    | action == Main.Left || action == Main.Right =
+        let moved_player = action_map player action
+            illegal_move = is_player_out_of_bounds moved_player board || is_player_on_brick board moved_player
+        in if illegal_move then player else moved_player
+    | otherwise = action_map player action
+
+action_map :: Player -> Action -> Player
+
+action_map (Player (Point row col) shape) Main.Left = Player (Point row (col-1)) shape
+action_map (Player (Point row col) shape) Main.Right = Player (Point row (col+1)) shape
+action_map (Player (Point row col) shape) Main.Up = Player (Point (row-1) col) shape
+action_map (Player (Point row col) shape) Main.Down = Player (Point (row+1) col) shape
+action_map player None = player
 
 display_board :: [[Bool]] -> String
 display_board board = foldl (\acc row -> acc ++ "\n" ++ (draw_row row)) "" board
@@ -128,13 +137,17 @@ display_board board = foldl (\acc row -> acc ++ "\n" ++ (draw_row row)) "" board
 draw_row :: [Bool] -> String
 draw_row row = foldl (\row pixel -> row ++ (if pixel == True then "O" else ".")) "" row
 
-is_game_over :: [[Bool]] -> Player -> Bool
-is_game_over board (Player player_point shape) =
-    any (\(row, rowi) -> is_player_on_brick row rowi player_point board) (zip shape [0..])
+is_player_on_brick :: [[Bool]] -> Player -> Bool
+is_player_on_brick board (Player player_point shape) =
+    any (\(row, rowi) -> is_player_on_brick_row row rowi player_point board) (zip shape [0..])
 
-is_player_on_brick :: [Bool] -> Int -> Point -> [[Bool]] -> Bool
-is_player_on_brick row rowi (Point player_row player_col) board =
+is_player_on_brick_row :: [Bool] -> Int -> Point -> [[Bool]] -> Bool
+is_player_on_brick_row row rowi (Point player_row player_col) board =
     any (\(pixel, coli) -> if pixel
                             then (board!!(player_row+rowi))!!(player_col+coli)
                             else False
         ) (zip row [0..])
+
+is_player_out_of_bounds :: Player -> [[Bool]] -> Bool
+is_player_out_of_bounds (Player (Point row col) shape) board =
+    col + length (shape!!0) > cols || col < 0
